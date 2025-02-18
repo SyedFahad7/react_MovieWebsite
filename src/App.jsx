364f816d0293a1462b278a973d4f8110e8a78cc2
Mohd Mujahed <sfahad2904@gmail.com>
2025-02-19 03:22:17 +0530

    implement MovieContext for favorites management; refactor components to utilize context for favorite movies

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import {Routes, Route} from 'react-router-dom'
 import Favourites from './pages/Favourites'
 import Navbar from './components/Navbar'
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
 
   return (
     <>
     <div>
+    <MovieProvider>
       <Navbar/>
       <main className='main-content'>
         <Routes>
@@ -16,6 +18,7 @@ function App() {
           <Route path='/favourites' element={< Favourites/>}/>
         </Routes>
       </main>
+      </MovieProvider>
     </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import MovieCard from "../components/MovieCard";
 import { useState, useEffect } from "react";
-import "../css/Home.css";
 import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
+
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
@@ -15,61 +16,60 @@ function Home() {
         setMovies(popularMovies);
       } catch (err) {
         console.log(err);
-        setError("Error loading popular movies");
+        setError("Failed to load movies...");
       } finally {
         setLoading(false);
       }
     };
+
     loadPopularMovies();
   }, []);
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    if (!searchQuery.trim()){
-        return
-    }
-    if (loading) return;
+    if (!searchQuery.trim()) return
+    if (loading) return
+
     setLoading(true)
     try {
-     const searchResults = await searchMovies(searchQuery)
-     setMovies(searchResults)
-     setError(null)
-    } catch (error) {
-      console.log(error);
-      setError("Error failed to load search");  
-    }
-    finally {
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    } catch (err) {
+        console.log(err)
+        setError("Failed to search movies...")
+    } finally {
         setLoading(false)
     }
   };
 
   return (
-    <>
-      <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-          <input
-            type="text"
-            placeholder="Search Movies.."
-            className="search-input"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button type="submit" className="search-btn">
-            Search
-          </button>
-        </form>
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
         {error && <div className="error-message">{error}</div>}
-        {loading ? (
-          <div className="loading">Loading....</div>
-        ) : (
-          <div className="movies-grid">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} />
-            ))}
-          </div>
-        )}
-      </div>
-    </>
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
